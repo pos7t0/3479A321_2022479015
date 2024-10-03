d820f3d77d1535a4021fea152b47358e0ccf8fd7
pos7t0 <74116675+pos7t0@users.noreply.github.com>
2024-10-03 10:23:51 -0300

    lab listo

--- lib/pages/MyHomeState.dart
+++ lib/pages/MyHomeState.dart
@@ -1,22 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:logger/logger.dart';
+import 'detail_page.dart'; // Importar DetailPage
 
-var logger = Logger(
-  printer: PrettyPrinter(),
-);
+final logger = Logger();
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -26,146 +16,152 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  
+  bool _hasLogged = false;
+
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
+      logger.d("Counter incremented to $_counter");
     });
   }
 
-  void _decreaseCounter(){
-
+  void _decrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter--;
+      if (_counter > 0) _counter--;
+      logger.d("Counter decremented to $_counter");
     });
-}
-  void _restartCounter(){
+  }
 
+  void _resetCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter=0;
+      _counter = 0;
+      logger.d("Counter reset to $_counter");
     });
-}
+  }
 
-  @override
-  Widget build(BuildContext context) {
-    logger.i('Logger is working');
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'has pulsado el boton un total de:',
-            ),
-            SvgPicture.asset(
-  "assets/icon/food_mango.svg",
-  semanticsLabel: 'Acme Logo',
-  width: 100,
-),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            
-          ],
+  // Función para determinar el mensaje y el ícono a mostrar
+  Widget _getGameStatus() {
+    String message;
+    Widget icon;
+
+    // Asignar mensaje e ícono dependiendo del valor del contador
+    if (_counter == 10) {
+      message = "¡Victoria!";
+      icon = SvgPicture.asset(
+        'assets/icon/win.svg',
+        width: 100,
+        height: 100,
+        semanticsLabel: 'Victoria Icon',
+      );
+    } else if (_counter == 5) {
+      message = "Derrota";
+      icon = SvgPicture.asset(
+        'assets/icon/game_over.svg',
+        width: 100,
+        height: 100,
+        semanticsLabel: 'Derrota Icon',
+      );
+    } else {
+      message = "¡Sigue jugando!";
+      icon = SvgPicture.asset(
+        'assets/icon/food_mango.svg',
+        width: 100,
+        height: 100,
+        semanticsLabel: 'bot_icon',
+      );
+    }
+
+    // Devolver tanto el mensaje como el ícono en una columna
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        icon, // Mostrar el ícono
+        const SizedBox(height: 16),
+        Text(
+          message,
+          style: const TextStyle(
+            fontSize: 24,
+            color: Colors.black,
+          ),
         ),
-      ),
-
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          INcrement(),
-
-          Decrease(),
-
-          Restart(), 
-        
-        ],
-        
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-      
+      ],
     );
   }
 
-  FloatingActionButton Restart() {
-    return FloatingActionButton(
-          onPressed: _restartCounter,
-          tooltip:'Reiniciar',
-          shape:RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(50.0),
-          ),
-          child: const Icon(Icons.restart_alt),
+  @override
+  Widget build(BuildContext context) {
+    if (!_hasLogged) {
+      logger.i("Building MyHomePage");
+      _hasLogged = true;
+    }
 
-        );
-  }
-
-  FloatingActionButton Decrease() {
-    return FloatingActionButton(
-          onPressed: _decreaseCounter,
-          tooltip:'disminuir',
-          shape:RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(50.0),
-          ),
-          child: const Icon(Icons.exposure_minus_1),
-
-        );
-  }
-
-  FloatingActionButton INcrement() {
-    return FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip:'Incrementa',
-          shape:RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(50.0),
-          ),
-          child: const Icon(Icons.plus_one),
-
-        );
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            // Card con el estado del juego y el contador
+            Card(
+              color: const Color.fromARGB(0, 0, 0, 0), // Color de la Card
+              elevation: 1, // Elevación de la Card
+              margin: const EdgeInsets.all(16),
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    // Mensaje del juego (Victoria, Derrota o Sigue jugando)
+                    _getGameStatus(),
+                    const SizedBox(height: 16),
+                    Text(
+                      '$_counter',
+                      style: const TextStyle(
+                        fontSize: 40,
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    // Botones para incrementar, decrementar y resetear
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          onPressed: _decrementCounter,
+                          icon: const Icon(Icons.arrow_downward),
+                        ),
+                        IconButton(
+                          onPressed: _resetCounter,
+                          icon: const Icon(Icons.refresh),
+                        ),
+                        IconButton(
+                          onPressed: _incrementCounter,
+                          icon: const Icon(Icons.arrow_upward),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            // Botón fuera de la Card
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const DetailPage(),
+                  ),
+                ); // Navegar a DetailPage
+              },
+              child: const Text('Go to Detail Page'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
